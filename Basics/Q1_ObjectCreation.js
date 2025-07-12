5e00d9d61aea52fcdca6c2b50afee5133865c5c3
chandanmutyala <chandanmutyala@gmail.com>
2025-07-12 15:29:21 +0530

    Created folders and one file

---
+++ Basics/Q1_ObjectCreation.js
@@ -0,0 +1,4 @@
+// Question :  Create an object with keys name, age, and city, and print all values.
+
+const object = { "name" : "chandan", "age" : 56 , "city" : "Hyderabad" };
+console.log(object);
